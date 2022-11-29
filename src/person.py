2ead4b330d8843fc0dbb5f8f1ecc96ebeb60ba6c
Rio <riobeggs1@gmail.com>
2022-11-30 11:43:55 +1300

    added class instance variables

---
+++ src/person.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self):
+        self._id = None
+        self._first_name = None
+        self._last_name = None
+        self._phone_number = None
+        self._email_address = None
+        self._shipping_id = None
+        self._billing_id = None
+        self._cart_id = None
