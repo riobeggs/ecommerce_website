d4b70dc37abefb3b85084447f473249dc955eace
Rio <riobeggs1@gmail.com>
2022-11-30 13:36:02 +1300

    added docstring and address dictionary setup to updatebiling()

--- src/person.py
+++ src/person.py
@@ -8,3 +8,21 @@ class Person:
         self._shipping_id = None
         self._billing_id = None
         self._cart_id = None
+
+    def UpdateBilling() -> None:
+        """
+        Person can create or update an existing
+        billing address which will be stored in a dictionary.
+
+        A unique ID will be created and assigned to
+        persons billing address.
+        """
+        billing_address = {
+            "first_name": None,
+            "last_name": None,
+            "address": None,
+            "post_code": None,
+            "city": None,
+            "country": None,
+            "phone_number": None,
+        }
