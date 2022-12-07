4ba7441339f7b2d8bdc83b825d3d1e40624d11cb
Rio <riobeggs1@gmail.com>
2022-12-07 19:26:08 +1300

    skeleton

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=("GET", "POST"))
 def home():
-    pass
+    if request.method == "GET":
+
+        return render_template("home.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
