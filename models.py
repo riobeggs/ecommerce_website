4fd9564c08ff1940f8df6702e52d5e98ebd78aac
Rio <riobeggs1@gmail.com>
2022-12-13 23:47:35 +1300

    refactored foreign_keys and id names

--- models.py
+++ models.py
@@ -7,20 +7,29 @@ Base = declarative_base()
 class Person(Base):
     __tablename__ = "person"
 
-    id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, primary_key=True)
     first_name = Column(String(32))
     last_name = Column(String(32))
     phone_number = Column(String(16))
     email_address = Column(String(64))
 
-    shipping_id = Column(Integer, ForeignKey("address.id"))
-    billing_id = Column(Integer, ForeignKey("address.id"), nullable=True)
+    shipping_id = Column(Integer, ForeignKey("address.address_id"))
+    billing_id = Column(
+        Integer,
+        ForeignKey("address.address_id"),
+        nullable=True,
+    )
+    shopping_cart_id = Column(
+        Integer,
+        ForeignKey("shopping_cart.shopping_cart_id"),
+        nullable=True,
+    )
 
 
 class Address(Base):
     __tablename__ = "address"
 
-    id = Column(Integer, primary_key=True)
+    address_id = Column(Integer, primary_key=True)
     street = Column(String(64))
     suburb = Column(String(64), nullable=True)
     city = Column(String(64))
@@ -31,26 +40,26 @@ class Address(Base):
 class Order(Base):
     __tablename__ = "order"
 
-    id = Column(Integer, primary_key=True)
-    total = Column(Integer)
+    order_id = Column(Integer, primary_key=True)
 
-    person_id = Column(Integer, ForeignKey("person.id"))
-    shipping_id = Column(Integer, ForeignKey("address.id"))
+    person_id = Column(Integer, ForeignKey("person.person_id"))
+    shipping_id = Column(Integer, ForeignKey("address.address_id"))
 
 
 class OrderItems(Base):
     __tablename__ = "order_items"
 
-    id = Column(Integer, primary_key=True)
-    order_id = Column(Integer, ForeignKey("order.id"))
-    item_id = Column(Integer, ForeignKey("item.id"))
+    order_items_id = Column(Integer, primary_key=True)
     quantity = Column(Integer)
+
+    order_id = Column(Integer, ForeignKey("order.order_id"))
+    item_id = Column(Integer, ForeignKey("item.item_id"))
 
 
 class Item(Base):
     __tablename__ = "item"
 
-    id = Column(Integer, primary_key=True)
+    item_id = Column(Integer, primary_key=True)
     title = Column(String(64))
     price = Column(Integer)
 
@@ -59,21 +68,28 @@ class Inventory(Base):
     __tablename__ = "inventory"
 
     id = Column(Integer, primary_key=True)
-    item_id = Column(Integer, ForeignKey("item.id"))
     stock = Column(Integer)
+
+    item_id = Column(Integer, ForeignKey("item.item_id"))
 
 
 class ShoppingCart(Base):
     __tablename__ = "shopping_cart"
 
-    id = Column(Integer, primary_key=True)
-    person_id = Column(Integer, ForeignKey("person.id"))
+    shopping_cart_id = Column(Integer, primary_key=True)
+
+    person_id = Column(Integer, ForeignKey("person.person_id"))
 
 
 class ShoppingCartItems(Base):
     __tablename__ = "shopping_cart_items"
 
-    id = Column(Integer, primary_key=True)
-    shopping_cart_id = Column(Integer, ForeignKey("shopping_cart.id"))
-    item_id = Column(Integer, ForeignKey("item.id"))
+    shopping_cart_items_id = Column(Integer, primary_key=True)
     quantity = Column(Integer)
+
+    shopping_cart_id = Column(
+        Integer,
+        ForeignKey("shopping_cart.shopping_cart_id"),
+        nullable=True,
+    )
+    item_id = Column(Integer, ForeignKey("item.item_id"))
