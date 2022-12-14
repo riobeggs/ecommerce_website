8af21ba1723c2114693dc191c773df2f949813b1
Rio <riobeggs1@gmail.com>
2022-12-14 15:00:14 +1300

    Create Address * Data is captured through create_address.html form * Data is added to Address table

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
 from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+import models
+
+engine = create_engine("sqlite:///test.sqlite", echo=True, future=True)
+
+models.Base.metadata.create_all(bind=engine)
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=("GET", "POST"))
-def home():
-    if request.method == "GET":
+@app.route("/", methods=["GET", "POST"])
+def create_address():
+    if request.method == "POST":
 
-        return render_template("home.html")
+        with Session(engine) as session:
+
+            user = models.Address(
+                street=request.form["street"],
+                suburb=request.form["suburb"],
+                city=request.form["city"],
+                postal_code=request.form["postal_code"],
+                country=request.form["country"],
+            )
+
+            session.add(user)
+
+            session.commit()
+
+            return "Success"
+
+    return render_template("create_address.html")
 
 
 if __name__ == "__main__":
